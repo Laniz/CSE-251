17f9e4a0e75fd853968723547308789f2a6e31cc
Shepherd Ncube <68825578+Laniz@users.noreply.github.com>
2025-02-03 23:15:01 -0700

    last changes

--- lesson_04/prove/prove.py
+++ lesson_04/prove/prove.py
@@ -2,21 +2,16 @@ import time
 import threading
 import random
 from datetime import datetime
-
-# Include cse 251 common Python files
 from cse251 import *
 
 # Global Constants - DO NOT CHANGE
-cars_to_be_made = 500
+CARS_TO_PRODUCE = 500
 MAX_QUEUE_SIZE = 10
 SLEEP_REDUCE_FACTOR = 50
 
-# NO GLOBAL VARIABLES!
-
 class Car():
-    """ This is the Car class that will be created by the factories """
+    """ Represents a randomly generated car. """
 
-    # Class Variables
     car_makes = ('Ford', 'Chevrolet', 'Dodge', 'Fiat', 'Volvo', 'Infiniti', 'Jeep', 'Subaru', 
                 'Buick', 'Volkswagen', 'Chrysler', 'Smart', 'Nissan', 'Toyota', 'Lexus', 
                 'Mitsubishi', 'Mazda', 'Hyundai', 'Kia', 'Acura', 'Honda')
@@ -28,110 +23,120 @@ class Car():
     car_years = [i for i in range(1990, datetime.now().year)]
 
     def __init__(self):
-        # Make a random car
+        """ Initializes a new car with random attributes. """
         self.model = random.choice(Car.car_models)
         self.make = random.choice(Car.car_makes)
         self.year = random.choice(Car.car_years)
 
-        # Sleep a little.  Last statement in this for loop - don't change
-        time.sleep(random.random() / (SLEEP_REDUCE_FACTOR))
+        time.sleep(random.random() / SLEEP_REDUCE_FACTOR)  # Simulate production delay
+        print(f'Created: {self.info()}')  # Print car details
 
-        # Display the car that has was just created in the terminal
-        print(f'Created: {self.info()}')
-           
     def info(self):
-        """ Helper function to quickly get the car information. """
+        """ Returns a string representation of the car. """
         return f'{self.make} {self.model}, {self.year}'
 
 
 class Queue251():
-    """ This is the queue object to use for this assignment. Do not modify!! """
+    """ Custom queue implementation for this assignment. """
 
     def __init__(self):
         self.__items = []
 
     def size(self):
+        """ Returns the current queue size. """
         return len(self.__items)
 
     def put(self, item):
-        assert len(self.__items) <= 10
+        """ Adds an item to the queue. """
         self.__items.append(item)
 
     def get(self):
+        """ Removes and returns the front item from the queue. """
         return self.__items.pop(0)
 
 
 class Factory(threading.Thread):
-    """ This is a factory.  It will create cars and place them on the car queue """
+    """ Factory thread that creates cars and adds them to the queue. """
 
-    def __init__(self, queue, banana1, sem_full):
+    def __init__(self, queue, banana1, banana2):
         threading.Thread.__init__(self)
         self.queue = queue
         self.banana1 = banana1
-        # banana1 is a semaphore it counts the number of available slots in the queue
-        self.sem_full = sem_full
+        self.banana2 = banana2
 
     def run(self):
-        for _ in range(cars_to_be_made):
-            self.banana1.acquire()  
+        """ Produces cars and adds them to the queue. """
+        for _ in range(CARS_TO_PRODUCE):
+            self.banana1.acquire() 
             car = Car()
-            self.queue.put(car)
-            self.sem_full.release()  
+            self.queue.put(car) 
+            self.banana2.release()
+
         
+        self.banana1.acquire()
+        self.queue.put(None)  
+        self.banana2.release()
+
 
 class Dealer(threading.Thread):
-    """ This is a dealer that receives cars """
+    """ Dealer thread that retrieves cars from the queue and sells them. """
 
-    def __init__(self, queue, banana1, sem_full, queue_stats):
+    def __init__(self, queue, banana1, banana2, queue_stats):
         threading.Thread.__init__(self)
         self.queue = queue
         self.banana1 = banana1
-        self.sem_full = sem_full
+        self.banana2 = banana2
         self.queue_stats = queue_stats
 
     def run(self):
-        for _ in range(cars_to_be_made):
-            self.sem_full.acquire()  # Wait for a car to be available
-            car = self.queue.get()
+        """ Sells cars and tracks queue stats. """
+        while True:
+            self.banana2.acquire()  #
+            car = self.queue.get()  
+
+            if car is None:  # Stop signal received
+                break
+
             print(f'Sold: {car.info()}')
-            self.queue_stats[self.queue.size()] += 1  # Track queue size
-            self.banana1.release()  # Signal that a slot is available
-            time.sleep(random.random() / (SLEEP_REDUCE_FACTOR))
+            
+            if self.queue.size() < MAX_QUEUE_SIZE:
+                self.queue_stats[self.queue.size()] += 1  # Track queue size
+
+            self.banana1.release()  
+            time.sleep(random.random() / SLEEP_REDUCE_FACTOR)  # Simulate selling delay
 
 
 def main():
+    """ Main function that sets up the factory and dealer. """
     log = Log(show_terminal=True)
 
-    # Create semaphores
-    banana1 = threading.Semaphore(MAX_QUEUE_SIZE)  # Controls factory production
-    sem_full = threading.Semaphore(0)  # Controls dealer sales
+    # Semaphores
+    banana1 = threading.Semaphore(MAX_QUEUE_SIZE)  
+    banana2 = threading.Semaphore(0)  
 
-    # Create queue251 
+    # Shared queue
     queue = Queue251()
+    queue_stats = [0] * MAX_QUEUE_SIZE  
 
-    # This tracks the length of the car queue during receiving cars by the dealership
-    # i.e., update this list each time the dealer receives a car
-    queue_stats = [0] * MAX_QUEUE_SIZE
-
-    # Create factory and dealership threads
-    factory = Factory(queue, banana1, sem_full)
-    dealer = Dealer(queue, banana1, sem_full, queue_stats)
+    # Factory and dealer threads
+    factory = Factory(queue, banana1, banana2)
+    dealer = Dealer(queue, banana1, banana2, queue_stats)
 
     log.start_timer()
 
-    # Start factory and dealership threads
     factory.start()
     dealer.start()
 
-    # Wait for factory and dealership to complete
     factory.join()
     dealer.join()
 
     log.stop_timer(f'All {sum(queue_stats)} cars have been created and sold.')
 
-    xaxis = [i for i in range(0, MAX_QUEUE_SIZE)]
+    # Graph queue stats
+    xaxis = [i for i in range(MAX_QUEUE_SIZE)]
     plot = Plots()
-    plot.bar(xaxis, queue_stats, title=f'{sum(queue_stats)} Produced: Count VS Queue Size', x_label='Queue Size', y_label='Count', filename='Production count vs queue size.png')
+    plot.bar(xaxis, queue_stats, title=f'{sum(queue_stats)} Produced: Count VS Queue Size', 
+             x_label='Queue Size', y_label='Count', filename='Production_count_vs_queue_size.png')
 
 
 if __name__ == '__main__':
